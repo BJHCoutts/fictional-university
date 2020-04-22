5ec48631b71a986e911bf384955d0b79a716916d
Brian Coutts <bjhcoutts@gmail.com>
2020-04-22 14:19:27 -0700

    basic wp functions testing

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-<?php $siteName = Fictional University ?>
-<h1><?php echo $siteName ?></h1>
+<h1><?php bloginfo("name"); ?></h1>
+<?php 
+	while(have_posts()) {
+		the_post(); ?>
+		<section>
+		<h2><?php the_title(); ?></h2>
+		<p><?php the_content(); ?></p>
+		<hr>
+		</section>
+		<?php
+	}
+?>
+
+<h1> 
+
+</h1>
