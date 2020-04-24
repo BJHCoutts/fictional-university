f6bccbcf0a99c2029c928bafc7e035f9706be345
Brian Coutts <bjhcoutts@gmail.com>
2020-04-23 18:13:09 -0700

    css and wp UI header added

--- footer.php
+++ footer.php
@@ -1,1 +1,4 @@
 <H1>This is the Footer</H1>
+<?php wp_footer(); ?>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,2 +1,8 @@
-<h1><?php bloginfo("name"); ?></h1>
-<h1>this is the header</h1>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php wp_head(); ?>
+	</head>
+	<body>
+		<h1><?php bloginfo("name"); ?></h1>
+		<h1>this is the header</h1>
